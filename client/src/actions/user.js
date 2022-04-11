d86af7779813d1bd4062b075c42e79eaf0353e8c
awqsomee <awqsome@bk.ru>
2022-04-12 02:56:58 +0300

    RegistrationForm

---
+++ client/src/actions/user.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const serverAddress = "http://localhost:5000";
+
+export const registration = async (name, surname, email, password) => {
+    try {
+        console.log(`${serverAddress}/api/auth/registration`);
+        const response = await axios.post(`${serverAddress}/api/auth/registration`, {
+            email,
+            password,
+            name,
+            surname,
+        });
+        alert(response.data.message);
+    } catch (e) {
+        alert(e);
+    }
+};
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Navbar from "./navbar/Navbar.jsx";
-import "./navbar/navbar.css";
-import "./app.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./navbar/Navbar.jsx";
+import "./app.css";
+import "./navbar/navbar.css";
+import Registration from "./registration/Registration.jsx";
 
 function App() {
     return (
@@ -10,7 +11,7 @@ function App() {
             <div className="app">
                 <Navbar />
                 <Routes>
-                    <Route path="/registratoin" component="Registration" />
+                    <Route path="/registration" element={<Registration />} />
                 </Routes>
             </div>
         </BrowserRouter>
