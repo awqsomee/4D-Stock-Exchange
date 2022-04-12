7b2c1b50f061a971853423f8fe5876b70f61fa45
awqsomee <awqsome@bk.ru>
2022-04-12 14:35:04 +0300

    FooterImported

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,6 +5,7 @@ import './app.css';
 import './navbar/navbar.css';
 import Registration from './registration/Registration.jsx';
 import Login from './login/Login.jsx';
+import Footer from './footer/Footer.jsx';
 
 function App() {
   return (
