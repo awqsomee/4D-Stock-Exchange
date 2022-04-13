241b384102c63bfa79abcad401d0c43f674e4aa9
julya15 <71527585+julya15@users.noreply.github.com>
2022-04-13 13:30:07 +0300

    account

--- client/src/components/account/Account.jsx
+++ client/src/components/account/Account.jsx
@@ -1,47 +1,40 @@
 import React from 'react'
-import Icon from '../../Icons/user_profile.svg'
+import './account.css'
 
 const Account = () => {
   return (
     <div className="account">
-      <div className="container_account">
-        <div className="container__content">
-          <div className="account__user_profile">
-            {' '}
-            <img src={user_profile} alt="user_profile_img" className="user_profile_img" />{' '}
-          </div>
-          <div className="account__nameUser"> Иван Иванов </div>
-        </div>
+      <div className="account__header"> Профиль</div>
+      <div className="account_container">
+        <div className="account__name"> Иконка</div>
+        <div className="account__nameUser"> Иван Иванов </div>
       </div>
-      <div className="container_account">
-        <div className="container__content">
-          <div className="account__name"> Имя: </div>
-          <div className="account__nameUser"> Иван </div>
-          <div className="account__name"> Фамилия: </div>
-          <div className="account__nameUser"> Иванов </div>
-          <div className="account__name"> Дата рождения: </div>
-          <div className="account__nameUser"> 01.02.1999 </div>
-        </div>
+
+      <div className="account_container">
+        <div className="account__name"> Имя: </div>
+        <div className="account__nameUser"> Иван </div>
+        <div className="account__name"> Фамилия: </div>
+        <div className="account__nameUser"> Иванов </div>
+        <div className="account__name"> Дата рождения: </div>
+        <div className="account__nameUser"> 01.02.1999 </div>
       </div>
-      <div className="container_account">
-        <div className="container__content">
-          <div className="account__name"> E-mail: </div>
-          <div className="account__nameUser"> Ivanov@mail.ru </div>
-          <div className="account__name"> Контактный номер: </div>
-          <div className="account__nameUser"> 8 (923) 123-45-67 </div>
-          <div className="account__name"> Паспортные данные: </div>
-          <div className="account__nameUser"> 01 23 456789 </div>
-        </div>
+
+      <div className="account_container">
+        <div className="account__name"> E-mail: </div>
+        <div className="account__nameUser"> Ivanov@mail.ru </div>
+        <div className="account__name"> Контактный номер: </div>
+        <div className="account__nameUser"> 8 (923) 123-45-67 </div>
+        <div className="account__name"> Паспортные данные: </div>
+        <div className="account__nameUser"> 01 23 456789 </div>
       </div>
-      <div className="container_account">
-        <div className="container__content">
-          <div className="account__name"> Платежная система: </div>
-          <div className="account__nameUser"> Visa </div>
-          <div className="account__name"> Срок действия карты: </div>
-          <div className="account__nameUser"> 12/2024 </div>
-          <div className="account__name"> Номер карты: </div>
-          <div className="account__name"> **** **** **** 1234 </div>
-        </div>
+
+      <div className="account_container">
+        <div className="account__name"> Платежная система: </div>
+        <div className="account__nameUser"> Visa </div>
+        <div className="account__name"> Срок действия карты: </div>
+        <div className="account__nameUser"> 12/2024 </div>
+        <div className="account__name"> Номер карты: </div>
+        <div className="account__nameUser"> **** **** **** 1234 </div>
       </div>
     </div>
   )
