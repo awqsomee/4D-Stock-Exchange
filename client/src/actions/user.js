c26dbe346166b91d6c8596ee881472f30e54707b
awqsomee <awqsome@bk.ru>
2022-04-13 06:12:20 +0300

    Auth

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,5 +1,6 @@
-import axios from 'axios';
-const serverAddress = 'https://gentle-sea-62964.herokuapp.com';
+import axios from 'axios'
+import { setUser } from '../reducers/userReducer'
+const serverAddress = 'https://gentle-sea-62964.herokuapp.com'
 
 export const registration = async (name, surname, email, password) => {
   try {
@@ -8,21 +9,40 @@ export const registration = async (name, surname, email, password) => {
       password,
       name,
       surname,
-    });
-    alert(response.data.message);
+    })
+    alert(response.data.message)
   } catch (e) {
-    alert(e.response.data.message);
+    alert(e.response.data.message)
   }
-};
+}
 
-export const login = async (email, password) => {
-  try {
-    const response = await axios.post(`${serverAddress}/api/auth/login`, {
-      email,
-      password,
-    });
-    alert(response.data.message);
-  } catch (e) {
-    alert(e.response.data.message);
+export const login = (email, password) => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(`${serverAddress}/api/auth/login`, {
+        email,
+        password,
+      })
+      dispatch(setUser(response.data.user))
+      localStorage.setItem('stonksToken', response.data.token)
+    } catch (e) {
+      alert(e.response.data.message)
+    }
   }
-};
+}
+
+export const auth = () => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(`${serverAddress}/api/auth/auth`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem('stonksToken')}` },
+      })
+      console.log(localStorage.getItem('stonksToken'))
+      dispatch(setUser(response.data.user))
+      localStorage.setItem('stonksToken', response.data.token)
+    } catch (e) {
+      alert(e.response.data.message)
+      localStorage.removeItem('stonksToken')
+    }
+  }
+}
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,17 +7,35 @@ import Registration from './registration/Registration.jsx'
 import Login from './login/Login.jsx'
 import Footer from './footer/Footer.jsx'
 import Content from './content-main-page/content/Content'
+import { useDispatch, useSelector } from 'react-redux'
+import { auth } from '../actions/user.js'
+import { useEffect } from 'react'
 
 function App() {
+  const isAuth = useSelector((state) => state.user.isAuth)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(auth())
+  }, [])
+
   return (
     <BrowserRouter>
       <div className="app">
         <Navbar />
-        <Routes>
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/stocks" element={<Content />} />
-        </Routes>
+        <div className="wrap">
+          {!isAuth && (
+            <Routes>
+              <Route path="/registration" element={<Registration />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          )}
+          {isAuth && (
+            <Routes>
+              <Route path="/stocks" element={<Content />} />
+            </Routes>
+          )}
+        </div>
         <Footer />
       </div>
     </BrowserRouter>
