67925fd9ca793dd1a92d462bbb600e886ceb5297
julya15 <71527585+julya15@users.noreply.github.com>
2022-04-15 01:23:38 +0300

    counter

--- client/src/components/stocks/Stock.jsx
+++ client/src/components/stocks/Stock.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Less from '../../assets/Icons/DashCircle.svg'
 import More from '../../assets/Icons/PlusCircle.svg'
 import Arrow from '../../assets/Icons/angle_down.svg'
@@ -9,6 +9,15 @@ const Stock = (props) => {
   // const symbol = response.data['1. symbol']
   // const name = response.data['2. name']
   // const price = response.data[price]
+  const [counter, setCounter] = useState([1])
+
+  function less() {
+    setCounter(counter - 1)
+  }
+
+  function more() {
+    setCounter(counter + 1)
+  }
 
   return (
     <div>
@@ -22,11 +31,15 @@ const Stock = (props) => {
         <div className="stock__change">+23%</div>
         <div className="stock__counter">
           <div className="stock__counter__less">
-            <img src={Less} alt="less_img" className="less_img"></img>{' '}
+            <button onClick={less}>
+              <img src={Less} alt="less_img" className="less_img"></img>{' '}
+            </button>
           </div>
-          <div className="count">1</div>
+          <div className="count">{counter}</div>
           <div className="stock__counter__more">
-            <img src={More} alt="more_img" className="more_img"></img>{' '}
+            <button onClick={more}>
+              <img src={More} alt="more_img" className="more_img"></img>{' '}
+            </button>
           </div>
         </div>
         <button className="button button__normal">Купить</button>
