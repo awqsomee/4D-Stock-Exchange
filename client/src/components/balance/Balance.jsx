041e2d24bbb39cdf04da757f25674bbf4c943449
nerpyshka <71436617+ValeriaNigametzianova@users.noreply.github.com>
2022-04-14 05:46:07 +0300

    0

--- client/src/components/balance/Balance.jsx
+++ client/src/components/balance/Balance.jsx
@@ -2,17 +2,28 @@ import React from 'react'
 import '../UI/buttons/buttons.css'
 import "./balance.css"
 import "../UI/input/input.css"
+import ModalBox from '../UI/ModalBox/ModalBox'
+import { useState } from 'react'
+import Input from "../UI/input/Input"
 
 const Balance = (props) => {
   // const symbol = response.data['1. symbol']
   // const name = response.data['2. name']
   // const price = response.data[price]
+  const [modalBox ,setModalBox]=useState(false)
+ 
+
+  // const [modalBoxInput, setModalBoxReg] = useState(false)
 
   return (
       <div className="balance">
-        <div className="balance__summ">1000 P</div>
+         <ModalBox visible={modalBox} setVisible={setModalBox}>
+            <Input className="search balance__input">  </Input>
+        </ModalBox>
+        <div className="balance__summ">{props.balance}</div>
         <input className="search balance__input"></input>
-        <button className="balance__button button button__normal">Пополнить</button>
+       
+        <button className="balance__button button button__normal" onClick={() => setModalBox(true)}>Пополнить</button>
       </div>
   )
 }
