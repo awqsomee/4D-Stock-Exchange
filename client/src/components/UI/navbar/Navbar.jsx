e10abc2c6635c37ea3ba144cb84b41ef1568b1f7
julya15 <71527585+julya15@users.noreply.github.com>
2022-04-15 03:36:02 +0300

    pointer

--- client/src/components/UI/navbar/Navbar.jsx
+++ client/src/components/UI/navbar/Navbar.jsx
@@ -31,8 +31,12 @@ const Navbar = (props) => {
       </ModalBox>
 
       <ModalBoxAcc visible={modalBoxAcc} setVisible={setModalBoxAcc}>
-      <div className="acc_pop_up"> <NavLink to="/account">Аккаунт</NavLink> </div>
-        <div className="acc_pop_up"
+        <div className="acc_pop_up">
+          {' '}
+          <NavLink to="/account">Аккаунт</NavLink>{' '}
+        </div>
+        <div
+          className="acc_pop_up"
           onClick={() => {
             dispatch(logout())
             setModalBoxAcc(false)
@@ -41,7 +45,6 @@ const Navbar = (props) => {
           Выйти
         </div>
       </ModalBoxAcc>
-
 
       <div className="container">
         <div className="navbar__logo">
