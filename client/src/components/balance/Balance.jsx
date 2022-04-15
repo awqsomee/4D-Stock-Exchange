26f7f3b69d6e60f6c80c307058d2a026c4b9399c
awqsomee <awqsome@bk.ru>
2022-04-15 05:08:35 +0300

    Squashed commit of the following:
    commit c8ad50b38770b7239a4d5bba19d89333052700ce
    Author: awqsomee <awqsome@bk.ru>
    Date:   Fri Apr 15 05:06:52 2022 +0300
    
        pz

--- client/src/components/balance/Balance.jsx
+++ client/src/components/balance/Balance.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
 import '../UI/buttons/buttons.css'
-import "./balance.css"
-import "../UI/input/input.css"
+import './balance.css'
+import '../UI/input/input.css'
 import InputBox from '../UI/ModalBox/inputBox/InputBox'
 import { useState } from 'react'
+import axios from 'axios'
+const serverAddress = 'https://gentle-sea-62964.herokuapp.com'
 
 const Balance = (props) => {
   // const symbol = response.data['1. symbol']
@@ -17,19 +19,36 @@ const Balance = (props) => {
   // const [buttonText, setButtonText] = useState(inputText)
 
   // useEffect(( ) => {
-  //   if (inputText==="" || inputText==="Пополнить" ) 
+  //   if (inputText==="" || inputText==="Пополнить" )
   //     setButtonText("Пополнить")
   //   else
   //     setButtonText(`${inputText}  ${currency}`)
   // }, [inputText])
   // const [modalBoxInput, setModalBoxReg] = useState(false)
-const currency = "USD"
+
+  const [balance, setBalance] = useState(0)
+
+  useEffect(() => {
+    getBalance()
+  }, [])
+
+  const getBalance = async () => {
+    try {
+      const responce = await axios.get(`${serverAddress}/api/auth/balance`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem('stonksToken')}` },
+      })
+      setBalance(responce.data['balanceUSD'])
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const currency = 'USD'
   return (
-      <div className="balance">
-        <div className="balance__summ" >{props.balance}</div>
-        <InputBox visible={modalBox} setVisible={setModalBox} currency={currency}>
-        </InputBox>
-      </div>
+    <div className="balance">
+      <div className="balance__summ">{`${balance} $`}</div>
+      <InputBox visible={modalBox} setVisible={setModalBox} currency={currency}></InputBox>
+    </div>
   )
 }
 
