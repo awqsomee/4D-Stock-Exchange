342364f2452330cb068ee0c8b05fafd05f426971
nerpyshka <71436617+ValeriaNigametzianova@users.noreply.github.com>
2022-09-30 00:54:06 +0300

    form's links, icons

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,15 +3,17 @@ import { setUser } from '../redux/slice'
 const serverAddress = 'https://gentle-sea-62964.herokuapp.com'
 // const serverAddress = 'http://localhost:5000'
 
-export const registration = (name, surname, email, password) => {
+export const registration = (name, email, password) => {
   return async (dispatch) => {
     try {
-      const response = await axios.post(`${serverAddress}/api/auth/registration`, {
-        email,
-        password,
-        name,
-        surname,
-      })
+      const response = await axios.post(
+        `${serverAddress}/api/auth/registration`,
+        {
+          email,
+          password,
+          name,
+        }
+      )
       dispatch(setUser(response.data.user))
       localStorage.setItem('stonksToken', response.data.token)
     } catch (e) {
@@ -39,7 +41,9 @@ export const auth = () => {
   return async (dispatch) => {
     try {
       const response = await axios.get(`${serverAddress}/api/auth`, {
-        headers: { Authorization: `Bearer ${localStorage.getItem('stonksToken')}` },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('stonksToken')}`,
+        },
       })
       dispatch(setUser(response.data.user))
       localStorage.setItem('stonksToken', response.data.token)
