5e450abfad7c45d7d35d42bcb2a6f26f0310db7b
nerpyshka <71436617+ValeriaNigametzianova@users.noreply.github.com>
2022-09-30 20:38:35 +0300

    stock + panel

--- src/components/panel/Panel.jsx
+++ src/components/panel/Panel.jsx
@@ -4,8 +4,9 @@ import './panel.css'
 const Panel = () => {
   return (
     <div className="container-panel">
+      <div className="ticker">Тикер</div>
       <div className="name">Наименование</div>
-      <div className="cost">Цена акции</div>
+      <div className="cost">Цена</div>
       <div className="change">Изменение</div>
     </div>
   )
