e92d5b5fd3903adbb93bc817bf22d4097cf6987b
nerpyshka <71436617+ValeriaNigametzianova@users.noreply.github.com>
2022-11-13 00:12:16 +0300

    Update History.jsx

--- src/components/history/History.jsx
+++ src/components/history/History.jsx
@@ -10,13 +10,17 @@ const History = (props) => {
       <div className="history__summ">{props.history.quantity}</div>
       <div className="history__price">
         {props.history.quantity &&
-          `${new Intl.NumberFormat('ru-RU').format(Number(props.history.price) * Number(props.history.quantity))} ${
-            props.history.currency
-          }`}
+          `${new Intl.NumberFormat('ru-RU').format(
+            Number(props.history.price) * Number(props.history.quantity)
+          )} ${props.history.currency}`}
         {!props.history.quantity &&
-          `${new Intl.NumberFormat('ru-RU').format(Number(props.history.price))} ${props.history.currency}`}
+          `${new Intl.NumberFormat('ru-RU').format(
+            Number(props.history.price)
+          )} ${props.history.currency}`}
       </div>
-      <div className="history__date">{props.history.date.toLocaleString('en-US')}</div>
+      <div className="history__date">
+        {props.history.date.toLocaleString('en-US')}
+      </div>
       {/* <div style={{ width: '30%', justifyContent: 'space-between' }}>
       </div> */}
     </div>
