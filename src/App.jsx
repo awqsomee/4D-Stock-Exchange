ae21c9a2914675f4e9e313d532b5abfe788d85b2
nerpyshka <71436617+ValeriaNigametzianova@users.noreply.github.com>
2022-11-19 23:23:26 +0300

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { auth } from './actions/auth'
@@ -14,6 +14,8 @@ import PageLoader from './components/UI/loader/PageLoader'
 function App() {
   const dispatch = useDispatch()
   const [isLoading, setIsLoading] = useState(true)
+  const [widthClient, setWidthClient] = useState()
+  const isAuth = useSelector((state) => state.toolkit.isAuth)
 
   useEffect(() => {
     if (localStorage.getItem('stonksToken')) {
@@ -22,7 +24,23 @@ function App() {
     } else setIsLoading(false)
   }, [])
 
-  const isAuth = useSelector((state) => state.toolkit.isAuth)
+  //считает ширину окна без прокрутки
+  const setWidthPage = () => {
+    let widthClient = window.innerWidth - document.documentElement.clientWidth
+    // setWidthClient(window.innerWidth - document.documentElement.clientWidth)
+    if (widthClient == 0) {
+      document.getElementById('root').style['padding-right'] = 0
+    }
+    return widthClient
+  }
+
+  //useMemo чтобы при каждом изменении ставились значения, не работает
+  //классу тоже не назначается значение
+  useEffect(() => {
+    setWidthPage()
+
+    console.log(window.innerWidth, document.documentElement.clientWidth, widthClient)
+  }, [document.documentElement.clientWidth])
 
   return (
     <BrowserRouter basename="/4D-Stock-Exchange">
@@ -32,25 +50,16 @@ function App() {
           <div className="wrap">
             {!isAuth && (
               <Routes>
-                <Route
-                  path="/stocks"
-                  element={<StockList title="Каталог акций" />}
-                />
+                <Route path="/stocks" element={<StockList title="Каталог акций" />} />
                 <Route path="*" element={<Navigate to="/stocks" />} />
               </Routes>
             )}
             {isAuth && (
               <Routes>
-                <Route
-                  path="/stocks"
-                  element={<StockList title="Каталог акций" />}
-                />
+                <Route path="/stocks" element={<StockList title="Каталог акций" />} />
                 <Route path="/account" element={<Account title="Профиль" />} />
                 <Route path="/wallet" element={<Currency title="Кошелек" />} />
-                <Route
-                  path="/portfolio"
-                  element={<Portfolio title="Ваши инвестиции" />}
-                />
+                <Route path="/portfolio" element={<Portfolio title="Ваши инвестиции" />} />
                 <Route path="*" element={<Navigate to="/stocks" />} />
               </Routes>
             )}
